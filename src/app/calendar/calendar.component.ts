35341da9895461d9db754d51009d5a180ff2ec71
Gábor Marczi <marczi.gabor@outlook.com>
2018-12-21 14:50:42 +0100

    style changes

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { CalendarEvent } from 'angular-calendar';
+import {
+  startOfDay,
+  endOfDay,
+  subDays,
+  addDays,
+  endOfMonth,
+  isSameDay,
+  isSameMonth,
+  addHours
+} from 'date-fns'
 
 @Component({
   selector: 'app-calendar',
@@ -10,6 +20,8 @@ export class CalendarComponent implements OnInit {
   
   viewDate :Date = new Date();
   @Output() selectedDate = new EventEmitter<Date>();
+  activeDayIsOpen: boolean = true;
+  view: string = 'month';
 
   constructor() {
   }
@@ -18,9 +30,24 @@ export class CalendarComponent implements OnInit {
   }
 
   dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
-    this.viewDate = date;
-    this.selectedDate.emit(this.viewDate);
-    console.log(date);
-    console.log(events);
+    if (isSameMonth(date, this.viewDate)) {
+      this.viewDate = date;
+      this.selectedDate.emit(this.viewDate);
+      if (
+        (isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
+        events.length === 0
+      ) {
+        this.activeDayIsOpen = false;
+      } else {
+        this.activeDayIsOpen = true;
+      }
+    }
   }
+
+  // dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
+  //   this.viewDate = date;
+  //   this.selectedDate.emit(this.viewDate);
+  //   console.log(date);
+  //   console.log(events);
+  // }
 }
